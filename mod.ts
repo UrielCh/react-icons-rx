ed3120c0b0098cf065e53bfad3b5ffe585dadd4b
urielch <uriel.chemouni@gmail.com>
2024-07-18 10:54:02 +0200

    update doc

--- mod.ts
+++ mod.ts
@@ -10,7 +10,7 @@
  * 
  * [See available icons here](https://react-icons.deno.dev/rx)
  * 
- * ## install packahe
+ * ## install the module
  * 
  * ```bash
  * deno add @preact-icons/rx
@@ -19,36 +19,15 @@
  * bunx jsr add @preact-icons/rx
  * ```
  * 
- * You may need to update your preact mapping to:
- *  - "preact": "npm:preact@10.22.1",
- *  - "preact/jsx-runtime": "npm:preact@10.22.1/jsx-runtime",
- *  - "preact/hooks": "npm:preact@10.22.1/hooks",
- * 
- * 
- * # Radix Icons icons for preact
- * 
- * [![JSR](https://jsr.io/badges/@preact-icons/rx)](https://jsr.io/@preact-icons/rx)
- * 
- * **License** [MIT](https://github.com/radix-ui/icons/blob/master/LICENSE)
- * 
- * **Project** [https://icons.radix-ui.com](https://icons.radix-ui.com)
- * 
- * [See available icons here](https://react-icons.deno.dev/rx)
- * 
- * ## install packahe
- * 
- * ```bash
- * deno add @preact-icons/rx
- * dnpx jsr add @preact-icons/rx
- * pnpm dlx jsr add @preact-icons/rx
- * bunx jsr add @preact-icons/rx
+ * You may need to update your preact mapping to avoid mixing JSR and http import:
+ * Currently Deno fresh import preact using https://esm.sh/preact http import can not be mixed with JSR package, so you may need to update your preact mapping:
+ * ```json
+ * {
+ *  "preact": "npm:preact@10.22.1",
+ *  "preact/jsx-runtime": "npm:preact@10.22.1/jsx-runtime",
+ *  "preact/hooks": "npm:preact@10.22.1/hooks",
+ * }
  * ```
- * 
- * You may need to update your preact mapping to:
- *  - "preact": "npm:preact@10.22.1",
- *  - "preact/jsx-runtime": "npm:preact@10.22.1/jsx-runtime",
- *  - "preact/hooks": "npm:preact@10.22.1/hooks",
- * 
  * 
  * ## import an icon from all icons
  * 
@@ -67,8 +46,8 @@
  * ```ts
  * import RxAccessibility from "react-icons/rx/RxAccessibility.ts"
  * ```
- * 
  * @module
+ * 
  */
 
 import { GenIcon } from "./deps.ts";
